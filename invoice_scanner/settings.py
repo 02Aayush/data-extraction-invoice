711ad732b730676946110006abe417e9febc0a53
Aayush R Gupta <02guptag@gmail.com>
2023-06-01 20:41:50 +0530

    Updated allowed host settings.py

--- invoice_scanner/settings.py
+++ invoice_scanner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&m1bq4#!6+g(-lj2i%c(@e%518!#$$-#sa%us*dlv#_s($9^&b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aayush02.pythonanywhere.com']
 
 
 # Application definition
